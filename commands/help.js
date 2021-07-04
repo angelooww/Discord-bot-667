5f11eda36a91a7a0f2a6bf24301667dcb11615be
Angel <73498695+angelooww@users.noreply.github.com>
2021-07-04 18:34:28 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
         .addField('⚠️ Information du bot :','```_info, _help```')
 		.addField('🎵 Musique :','```_p, _skip, _loop, _stop, _filter, _volume, _repeat, _pause, _queue, _resume```')
 		.addField('🎬 Recherche films,series,animes :','```_anime, _animeinfo, _film, _serie```')
-		.addField('✅ Autre :','```_clear, _avatar, _covid, _667```')
+		.addField('✅ Autre :','```_clear, _sondage, _avatar, _covid, _667, _youtube, _spotify```')
         
         .setTimestamp()
         .setFooter('Crédit : Le fantôme', 'https://i.ibb.co/RB6HtTh/3dgifmaker00976.gif');
