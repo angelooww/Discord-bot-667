6900288c1927a0561bf74f1cc51305e4bf9a1855
Angel <73498695+angelooww@users.noreply.github.com>
2023-01-25 20:45:38 +0100

    Add files via upload

---
+++ events/interactionCreate.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder, InteractionType } = require('discord.js');
+
+module.exports = (client, inter) => {
+    if (inter.type === InteractionType.ApplicationCommand) {
+        const DJ = client.config.opt.DJ;
+        const command = client.commands.get(inter.commandName);
+
+    if (!command) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription('❌ | Erreur! Veuillez contacter Angel_#2720 !')], ephemeral: true, }), client.slash.delete(inter.commandName)
+    if (command.permissions && !inter.member.permissions.has(command.permissions)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | Vous n'avez pas besoin d'avoir les autorisations appropriées pour exécuter cette commande`)], ephemeral: true, })
+    if (DJ.enabled && DJ.commands.includes(command) && !inter.member._roles.includes(inter.guild.roles.cache.find(x => x.name === DJ.roleName).id)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | Cette commande est réservée aux membres ayant \`${DJ.roleName}\` `)], ephemeral: true, })
+    if (command.voiceChannel) {
+            if (!inter.member.voice.channel) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | Vous n'êtes pas dans un canal vocal`)], ephemeral: true, })
+            if (inter.guild.members.me.voice.channel && inter.member.voice.channel.id !== inter.guild.members.me.voice.channel.id) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | Vous n'êtes pas dans le même canal vocal`)], ephemeral: true, })
+       }
+        command.execute({ inter, client });
+    }
+    if (inter.type === InteractionType.MessageComponent) {
+        const customId = JSON.parse(inter.customId)
+        const file_of_button = customId.ffb
+        const queue = player.getQueue(inter.guildId);
+        if (file_of_button) {
+            delete require.cache[require.resolve(`../src/buttons/${file_of_button}.js`)];
+            const button = require(`../src/buttons/${file_of_button}.js`)
+            if (button) return button({ client, inter, customId, queue });
+        }
+    }
+};
